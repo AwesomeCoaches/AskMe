1a8bbc0ffb8367e43306da2e868f5b9c865a67cb
henryhansollee <henryhansollee@gmail.com>
2021-02-09 15:33:36 +0900

    Create Backend Signup

--- backend/src/main/java/com/coach/askme/model/User.java
+++ backend/src/main/java/com/coach/askme/model/User.java
@@ -1,13 +1,19 @@
 package com.coach.askme.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
 public class User {
 
-    private String _id;
+    @Transient
+    public static final String SEQUENCE_NAME = "user_sequence";
+
+    @Id
+    private Long uid;
     private String name;
     private String password;
 }
