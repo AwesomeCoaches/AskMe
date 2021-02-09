bfdc29125be981205056ccb47296bb66f9b0607f
Chae Lin Shin <chaelinshin96@gmail.com>
2021-02-09 11:35:52 +0900

    Add signup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
 import Detail from '@/views/Detail.vue'
 import Login from '@/views/Login.vue'
+import Signup from '@/views/Signup.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   }
 ]
 
