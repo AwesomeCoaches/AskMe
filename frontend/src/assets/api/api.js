75907dd96a736572b222418a0fcb14ee2b29f672
Chae Lin Shin <chaelinshin96@gmail.com>
2021-02-16 12:44:16 +0900

    Update authentication (login, logout, signup) by adding axios

---
+++ frontend/src/assets/api/api.js
@@ -0,0 +1,8 @@
+export default {
+    URL: 'http://t4coach38.p.ssafy.io/askme',
+    ROUTES: {
+      signup: '/users/signup',
+      login: '/users/login',
+      questions: '/questions',
+    }
+  }
