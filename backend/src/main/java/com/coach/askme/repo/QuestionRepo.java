02236264b258b693d6a71958e4489178a9e2d1c9
henryhansollee <henryhansollee@gmail.com>
2021-02-16 16:53:18 +0900

    Add statistics query logic

--- backend/src/main/java/com/coach/askme/repo/QuestionRepo.java
+++ backend/src/main/java/com/coach/askme/repo/QuestionRepo.java
@@ -4,10 +4,12 @@ import com.coach.askme.model.Question;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface QuestionRepo extends MongoRepository<Question, Long> {
 
     Optional<Question> findByQid(Long qid);
+
 }
